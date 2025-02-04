913a7fef0060c1e1fe1a48be0e5e3d9ab4b388b6
Laurentzo509 <145812982+Laurentzo509@users.noreply.github.com>
2025-02-04 16:05:32 -0500

    Update set.js

--- set.js
+++ set.js
@@ -1,12 +1,12 @@
 global.prefix = '/'; // Set Your Preferred Prefix
-global.botName = 'GIFTED-MD'; // Bot Name
+global.botName = 'KAISEN-MD'; // Bot Name
 global.botVersion = '2.0.0';
 global.botPic = 'https://files.giftedtech.web.id/file/gifted-md.jpg'; // Can use path, can also use link
-global.botToken = 'xxxxxxxx:xxxx'; // Replace With Your Bot Token (Get it from BotFather)
-global.ownerUsername = 'mouricedevs'; // Replace With Your Telegram Username(For Bot Owner)
+global.botToken = '8151985863:AAFKqhTKa5wfxInIWNb_wQQ_vjan8Wlghdw'; // Replace With Your Bot Token (Get it from BotFather)
+global.ownerUsername = 'Kaisensudo'; // Replace With Your Telegram Username(For Bot Owner)
 global.timeZone = 'Africa/Nairobi'; // Your Time Zone
-global.ownerId = [xxxxxx]; // Replace With Your Owner ID(For Bot Owner), You Can Add More than 1
-global.footer = '*Powered by GiftedTech*';
+global.ownerId = [8043814503]; // Replace With Your Owner ID(For Bot Owner), You Can Add More than 1
+global.footer = '*Powered by Kaisen*';
 global.ytdlWeb = 'https://ytdl.giftedtech.web.id';
 global.giftedApi = 'https://api.giftedtech.web.id/api'; // Gifted Api
 global.giftedKey = ''; // Gifted Api Key
